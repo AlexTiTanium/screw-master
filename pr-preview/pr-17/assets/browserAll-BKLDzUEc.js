944027a261ea80ac6a644226cb9ecc65d080d718
AlexTiTanium <AlexTiTanium@users.noreply.github.com>
2026-01-13 14:08:59 +0000

    Deploy preview for PR 17 🛫

---
+++ pr-preview/pr-17/assets/browserAll-BKLDzUEc.js
@@ -0,0 +1,2 @@
+import{e as a,A as e,C as i,a as s,E as t,F as n,D as d}from"./pixi-v3tV1XBX.js";import"./webworkerAll-CGQhtSIc.js";a.add(e);a.mixin(i,s);a.add(t);a.mixin(i,n);a.add(d);
+//# sourceMappingURL=browserAll-BKLDzUEc.js.map
