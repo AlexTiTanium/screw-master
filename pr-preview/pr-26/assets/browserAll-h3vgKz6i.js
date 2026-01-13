94282840af59116267a1e4ce3c9c3e19cb120257
AlexTiTanium <AlexTiTanium@users.noreply.github.com>
2026-01-13 18:45:45 +0000

    Deploy preview for PR 26 🛫

---
+++ pr-preview/pr-26/assets/browserAll-h3vgKz6i.js
@@ -0,0 +1,2 @@
+import{e as a,A as e,C as i,a as s,E as t,F as n,D as d}from"./pixi-B6QmwNQM.js";import"./webworkerAll-fMf3vJ0n.js";a.add(e);a.mixin(i,s);a.add(t);a.mixin(i,n);a.add(d);
+//# sourceMappingURL=browserAll-h3vgKz6i.js.map
